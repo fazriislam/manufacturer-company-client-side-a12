c284c411211f6e8a50dee9356e5d662a33743225
Fazri Isalm <fazriislam.work@gmail.com>
2022-05-28 13:53:38 +0600

    Create Profile PAge & Its Modal

--- src/Pages/Dashboard/Profile.js
+++ src/Pages/Dashboard/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import Loading from '../Shared/Loading';
@@ -6,16 +6,30 @@ import ProfileModal from './ProfileModal';
 
 const Profile = () => {
     const [user] = useAuthState(auth);
+    // const [profileInfo, setProfileInfo] = useState([]);
     const [clicked, setClicked] = useState(false);
+
+
+
+    // useEffect(() => {
+    //     fetch(`http://localhost:5000/profile?email=${user?.email}`)
+    //         .then(res => res.json())
+    //         .then(data => setProfileInfo(data))
+    // }, [profileInfo]);
+    // console.log(profileInfo);
+
+
     if (!user) {
         return <Loading />
     }
     const { displayName, email, photoURL } = user;
 
+
+
     return (
         <div className='flex flex-col mt-3'>
             <section className='lg:ml-48 mx-auto'>
-                <img className='' src={photoURL} alt="" />
+                {photoURL && <img className='' src={photoURL} alt="" />}
                 <h2 className='font-bold' >{displayName}</h2>
                 <p><small className='font-semibold'>Email: {email}</small></p>
             </section>
@@ -25,11 +39,11 @@ const Profile = () => {
                     <legend className='font-bold'>User Info</legend>
                     <div className='border p-1 rounded'>
                         <p className='font-semibold'>Education</p>
-                        <p><small>dhfdfdjkfhdkshfjkdsfj</small></p>
+                        <p><small>ghdfkgjhdfjghfdjg</small></p>
                     </div>
                     <div className='border p-1 mt-1 rounded'>
                         <p className='font-semibold'>Location</p>
-                        <p><small>dhfdfdjkfhdkshfjkdsfj</small></p>
+                        <p><small>fgjkfdgjkdf</small></p>
                     </div>
                     <div className='border p-1 mt-1 rounded'>
                         <p className='font-semibold'>Phone Number</p>
@@ -41,9 +55,13 @@ const Profile = () => {
                 <label for="profile-modal" onClick={() => setClicked(true)} class="btn modal-button mx-auto lg:ml-48 block lg:w-1/12 w-2/5 pt-4">Edit Profile</label>
             </div>
 
-            {
-                clicked && <ProfileModal />
-            }
+            <div>
+                {
+                    clicked && <ProfileModal
+                        user={user}
+                    />
+                }
+            </div>
 
         </div>
     );
--- src/Pages/Dashboard/ProfileModal.js
+++ src/Pages/Dashboard/ProfileModal.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import { toast } from 'react-toastify';
 
-const ProfileModal = () => {
+const ProfileModal = ({ user }) => {
+
+    const { displayName, email, photoURL } = user;
+
+    const handleUpdateProfile = profileData => {
+        profileData.preventDefault();
+
+        const profileInfo = {
+            name: displayName,
+            email: email,
+            photo: profileData.target.photo.value,
+            education: profileData.target.photo.value,
+            location: profileData.target.location.value,
+            phone: profileData.target.phone.value,
+        }
+
+        // fetch(`http://localhost:5000/profile?email=${email}`, {
+        //         method: 'PATCH',
+        //         headers: {
+        //             'content-type': 'application/json'
+        //         },
+        //         body: JSON.stringify(profileInfo),
+        //     })
+        //         .then(res => res.json())
+        //         .then(data => {
+        //             if (data.success) {
+        //                 toast.success(`Your is updated`);
+        //             }
+
+        //         })
+
+    }
+
     return (
         <div>
             <input type="checkbox" id="profile-modal" class="modal-toggle" />
@@ -8,7 +41,7 @@ const ProfileModal = () => {
                 <div class="modal-box">
                     <label for="profile-modal" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                     <h3 class="font-bold text-lg">Edit Your Profile</h3>
-                    <form onSubmit={''}>
+                    <form onSubmit={handleUpdateProfile}>
                         <label htmlFor="photo">Profile Picture: </label>
                         <input type="file" name="photo" id="photo" />
 
@@ -17,14 +50,16 @@ const ProfileModal = () => {
                         <input type="text" placeholder='Enter Your Location' name='location' className="input text-xl input-bordered w-full max-w-xs mt-1" />
 
                         <input type="tel" placeholder='Enter Your Phone Number' name='phone' className="input text-xl input-bordered w-full max-w-xs mt-1" />
+
+                        <div class="modal-action">
+                            <input type="submit" value="Update" className='btn' />
+                        </div>
                     </form>
-                    <div class="modal-action">
-                        <label for="profile-modal" class="btn">Update</label>
-                    </div>
                 </div>
             </div>
         </div>
     );
+    // class="btn"
 };
 
 export default ProfileModal;
